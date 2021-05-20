258ec2ea6da926482c0ff1181e924985562d1a13
Chary Nepesov <ogr.n.chary@ahievran.edu.tr>
2021-05-20 10:24:46 +0300

    Added TestMethod

--- Tests/ACM.BLTest/CustomerRepositoryTest.cs
+++ Tests/ACM.BLTest/CustomerRepositoryTest.cs
@@ -1,5 +1,6 @@
 ﻿using ACM.BL;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 
 namespace ACM.BLTest
 {
@@ -26,6 +27,50 @@ namespace ACM.BLTest
             Assert.AreEqual(expected.EmailAddress, actual.EmailAddress);
             Assert.AreEqual(expected.FirstName, actual.FirstName);
             Assert.AreEqual(expected.LastName, actual.LastName);
+        }
+        [TestMethod]
+        public void RetrieveExistingWithAddress()
+        {
+            //-- Arrange
+            var customerRepository = new CustomerRepository();
+            var expected = new Customer(1)
+            {
+                EmailAddress = "fbaggins@hobbiton.me",
+                FirstName = "Frodo",
+                LastName = "Baggins",
+                AddressList = new List<Address>()
+                {
+                    new Address()
+                    {
+                        AddressType = 1,
+                        StreetLine1 = "Bag End",
+                        StreetLine2 = "Bagshot row",
+                        City = "Hobbiton",
+                        StateProvince = "Shire",
+                        Country = "Middle Earth",
+                        PostalCode = "144"
+                     }
+                }
+            };
+
+            //-- Act
+            var actual = customerRepository.Retrieve(1);
+
+            //-- Assert
+            Assert.AreEqual(expected.CustomerId, actual.CustomerId);
+            Assert.AreEqual(expected.EmailAddress, actual.EmailAddress);
+            Assert.AreEqual(expected.FirstName, actual.FirstName);
+            Assert.AreEqual(expected.LastName, actual.LastName);
+
+            for (int i = 0; i < 1; i++)
+            {
+                Assert.AreEqual(expected.AddressList[i].AddressType, actual.AddressList[i].AddressType);
+                Assert.AreEqual(expected.AddressList[i].StreetLine1, actual.AddressList[i].StreetLine1);
+                Assert.AreEqual(expected.AddressList[i].City, actual.AddressList[i].City);
+                Assert.AreEqual(expected.AddressList[i].StateProvince, actual.AddressList[i].StateProvince);
+                Assert.AreEqual(expected.AddressList[i].Country, actual.AddressList[i].Country);
+                Assert.AreEqual(expected.AddressList[i].PostalCode, actual.AddressList[i].PostalCode);
+            }
         }
     }
 }
