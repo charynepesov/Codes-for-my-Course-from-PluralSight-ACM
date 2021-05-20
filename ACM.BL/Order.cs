31d99dbcae5060aadd21afc014b808056f9a53e1
Chary Nepesov <ogr.n.chary@ahievran.edu.tr>
2021-05-20 10:27:08 +0300

    Added new properties on Order Class and revised constructor

--- ACM.BL/Order.cs
+++ ACM.BL/Order.cs
@@ -6,18 +6,22 @@ namespace ACM.BL
 {
     public class Order
     {
-        public Order()
+        public Order() : this(0)
         {
 
         }
         public Order(int orderId)
         {
             this.OrderId = orderId;
+            this.OrderItems = new List<OrderItem>();
         }
 
+        public int CustomerId { get; set; }
         public int OrderId { get; private set; }
         public DateTimeOffset? OrderDate { get; set; }
-                
+        public List<OrderItem> OrderItems { get; set; }
+        public int ShippingAddressId { get; set; }
+
         public bool Validate()
         {
             bool isValid = true;
